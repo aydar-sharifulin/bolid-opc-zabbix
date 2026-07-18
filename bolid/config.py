a57386de5e4213c32ff04714ed9298978615af3d
aydar-sharifulin <128936314+aydar-sharifulin@users.noreply.github.com>
2026-07-18 19:00:09 +0400

    Update config.py

--- bolid/config.py
+++ bolid/config.py
@@ -1,14 +1,21 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-OPC_URL = os.getenv("OPC_URL")
-OPC_ROOT_NODE = os.getenv("OPC_ROOT_NODE")
 
-ZABBIX_SERVER = os.getenv("ZABBIX_SERVER")
-ZABBIX_HOST = os.getenv("ZABBIX_HOST")
+def getenv(name: str) -> str:
+    value = os.getenv(name)
+    if not value:
+        raise RuntimeError(f"Environment variable '{name}' is not set")
+    return value
+
+
+OPC_URL = getenv("OPC_URL")
+OPC_ROOT_NODE = getenv("OPC_ROOT_NODE")
+
+ZABBIX_SERVER = getenv("ZABBIX_SERVER")
+ZABBIX_HOST = getenv("ZABBIX_HOST")
 
 DISCOVERY_KEY = os.getenv("DISCOVERY_KEY", "bolid.discovery")
-
 STATE_FILE = os.getenv("STATE_FILE", "last_states.json")
